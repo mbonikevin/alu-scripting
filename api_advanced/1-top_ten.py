cb0762ea882e59ebc78d66093c2124426fe6f2d9
mbonikevin <k.mbonimpay@alustudent.com>
2025-02-21 20:16:25 +0200

    Update 1-top_ten.py

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
-""" this will queries the API for reddit """
+""" This will query the API for reddit """
 import requests
-import sys
 
 
 def top_ten(subreddit):
-    """getting top ten post titles"""
-    
+    """ Getting top ten post titles """
+
     headers = {'User-Agent': 'MyAPI/0.0.1'}
-    subreddit_url = "https://reddit.com/r/{}.json".format(subreddit)
-    response = requests.get(subreddit_url, headers=headers)
+    subreddit_url = f"https://reddit.com/r/{subreddit}.json"
+
+    response = requests.get(
+        subreddit_url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         json_data = response.json()
-        for i in range(10):
-            print(
-                json_data.get('data')
-                .get('children')[i]
-                .get('data')
-                .get('title')
-            )
+        # Check if the 'children' key exists
+        if 'data' in json_data and 'children' in json_data['data']:
+            for i in range(10):
+                print(json_data['data']['children'][i]['data']['title'])
+        else:
+            print(None)
     else:
         print(None)
