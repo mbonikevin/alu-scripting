f8ba04d5696e350e270e0109489190dfa1153f72
mbonikevin <k.mbonimpay@alustudent.com>
2025-02-21 19:58:10 +0200

    0

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -5,13 +5,17 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36(by /u/Gaius2324)"}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """gets the number of subscribers for a subreddit"""
+    reddit_url = "https://www.reddit.com/r/{}/about.json" \
+        .format(subreddit)
+
+    header = {'User-agent': 'Mozilla/5.0'}
+    response = requests.get(reddit_url,
+                            headers=header
+                            )
+
     if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
-        return 0
+        data = response.json()['data']
+        subs = data['subscribers']
+        return subs
+    return 0
